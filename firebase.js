8fc31884cf7e5d98e60c2ff0a2e58d1d1797c0ab
NexusOrg-Collab <albertgrade9@gmail.com>
2025-06-02 23:11:20 +0800

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// firebase.js
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxkGWneCDuy1pD04YYTPp5OMY39yFeefE",
+  authDomain: "nexusauth-ee032.firebaseapp.com",
+  projectId: "nexusauth-ee032",
+  storageBucket: "nexusauth-ee032.appspot.com",
+  messagingSenderId: "828563806988",
+  appId: "1:828563806988:web:dfd152bb3f78c4421c729",
+  measurementId: "G-1HLXMS867J"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
